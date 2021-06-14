a5403f9e466325d10f36ef48097aad2d318a884f
kjinnes <kimberly.innes@gmail.com>
2021-06-14 19:16:26 -0400

    first commit

---
+++ pages/reduxexample.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Examples from '../components/examples'
+
+const reduxexample = () => {
+  return (
+    <div>
+      <Examples />
+    </div>
+  )
+}
+
+export default reduxexample
